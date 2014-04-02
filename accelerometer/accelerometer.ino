34a81ff287653ca228dfe232f5a47cb63d8a74c0
flohdot <floh@flohdot.org>
2014-04-02 12:00:17 -0400

    recalibrated, and using x instead of z axis

--- accelerometer/accelerometer.ino
+++ accelerometer/accelerometer.ino
@@ -8,8 +8,8 @@ int zPrev;
 
 /* zero values as determined w/ code below */
 int xStart = 330;
-int yStart = 335;
-int zStart = 328;
+int yStart = 326;
+int zStart = 403;
 
 int diff;
 
@@ -24,38 +24,38 @@ void loop() {
   xAcc = analogRead(A0);
   yAcc = analogRead(A1);
   zAcc = analogRead(A2);
-  zPrev = zStart;
+  xPrev = xStart;
 
 
   /*  print zero point when lying flat */
-//  Serial.print("xAcc ");
-//  Serial.println(xAcc);
-//  Serial.print("yAcc ");
-//  Serial.println(yAcc);
-//  Serial.print("zAcc ");
-//  Serial.println(zAcc);
-//  Serial.println(" ");
-//  delay(500);//wait
+ // Serial.print("xAcc ");
+ // Serial.println(xAcc);
+ // Serial.print("yAcc ");
+ // Serial.println(yAcc);
+ // Serial.print("zAcc ");
+ // Serial.println(zAcc);
+ // Serial.println(" ");
+ // delay(500);//wait
 //
 
-  diff = abs(zPrev-zAcc);
+  diff = abs(xPrev-xAcc);
 
-  /* inspect */
-//  Serial.print("xAcc ");
-//  Serial.println(xStart-xAcc);
-//  Serial.print("yAcc ");
-//  Serial.println(yStart-yAcc);
-//  Serial.print("zAcc ");
-//  Serial.println(zStart-zAcc);
-//
-//  Serial.print("diff is");
-//  Serial.println(diff);
-//  delay(500);//wait
+  // /* inspect */
+   // Serial.print("xAcc ");
+   // Serial.println(xStart-xAcc);
+   // Serial.print("yAcc ");
+   // Serial.println(yStart-yAcc);
+   // Serial.print("zAcc ");
+   // Serial.println(zStart-zAcc);
+
+   // Serial.print("DIFF IS ");
+   // Serial.println(diff);
+   // delay(2000);//wait
 
   /* set a random sensitivity for testing */
   if (diff > 40) {
     Serial.println(1);
-    zPrev = zAcc;
+    xPrev = xAcc;
   } else {
     Serial.println(0);
   }

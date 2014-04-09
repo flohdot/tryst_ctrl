907e0e88f9f04e156959e6c7a5162ae6c48835e9
flohdot <floh@flohdot.org>
2014-04-09 12:55:25 -0400

    "joystick" mode with NSEW triggers

--- accelerometer/accelerometer.ino
+++ accelerometer/accelerometer.ino
@@ -7,9 +7,12 @@ int yPrev;
 int zPrev;
 
 /* zero values as determined w/ code below */
-int xStart = 330;
-int yStart = 326;
-int zStart = 403;
+int xStart = 396;
+int yStart = 321;
+int zStart = 363;
+
+int trigger= 5;
+
 
 int diff;
 
@@ -35,30 +38,56 @@ void loop() {
  // Serial.print("zAcc ");
  // Serial.println(zAcc);
  // Serial.println(" ");
- // delay(500);//wait
+ // //wait
 //
 
-  diff = abs(xPrev-xAcc);
+  // diff = abs(xPrev-xAcc);
 
-  // /* inspect */
-   // Serial.print("xAcc ");
-   // Serial.println(xStart-xAcc);
-   // Serial.print("yAcc ");
-   // Serial.println(yStart-yAcc);
-   // Serial.print("zAcc ");
-   // Serial.println(zStart-zAcc);
+  // /* inspect raw values */
+//   Serial.print("xAcc ");
+//   Serial.println(xStart-xAcc);
+//  Serial.print("yAcc ");
+//   Serial.println(yStart-yAcc);
+//   Serial.print("zAcc ");
+//   Serial.println(zStart-zAcc);
 
-   // Serial.print("DIFF IS ");
-   // Serial.println(diff);
-   // delay(2000);//wait
+//   Serial.print("DIFF IS ");
+//   Serial.println(diff);
+//   delay(1000);//wait
+
+   /* end */
 
   /* set a random sensitivity for testing */
-  if (diff > 40) {
-    Serial.println(1);
-    xPrev = xAcc;
-  } else {
-    Serial.println(0);
-  }
+  // if (diff > 40) {
+  //   Serial.println(1);
+  //   xPrev = xAcc;
+  // } else {
+  //   Serial.println(0);
+  // }
+
+// south -45 y axis
+// east-45 
+// north +45 y axis
+// west +45
+
+
+   int current = 0;
+   if (yStart-yAcc < -40) {
+     current = 1; //east
+   } 
+   if (yStart-yAcc > 30) {
+     current = 2; //west
+   } 
+   if (zStart-zAcc < -25) {
+     current = 3; //south
+   } 
+   if (zStart-zAcc > 60) {
+     current = 4; //north
+   } 
+
+   Serial.println(current);
+  
+   delay(1000);//wait
 
 }
 
